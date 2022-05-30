8a273106c909fba52949c7ca7e61a0a85b64be93
OG4HUNNIT <william_tau@student.asdah.school.nzT>
2022-05-31 09:52:27 +1200

    created quiz start page

---
+++ starter_frame.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+
+quiz_a = ttk.Label(root, text="Quiz Time", wraplength=100, justify="center")
+quiz_a.grid(row=0, column=1, padx=450, pady=450)
+
+start_button = ttk.Button(root, text="Start")
+
+
+
+
+
+
+
+
+
+root.mainloop()
